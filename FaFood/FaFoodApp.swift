e78a4e2bdde5402791855482b22ed06131252394
PandaKris <kristantosean@gmail.com>
2022-09-08 11:53:37 +0700

    Initial Commit

---
+++ FaFood/FaFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaFoodApp.swift
+//  FaFood
+//
+//  Created by Kristanto Sean N on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FaFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
